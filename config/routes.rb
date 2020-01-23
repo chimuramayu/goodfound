5ac9704ba3a4d2de2350743dd1aab1a141612fe5
chimuramayu <chimuramayu@gmail.com>
2020-01-23 21:40:20 +0900

    [add]ログイン、サインアップ、ログアウト機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
+  get '/timeline', to: 'posts#index', as: :timeline
+  root 'home#top'
+  get '/about', to: 'home#about', as: :about
 end
