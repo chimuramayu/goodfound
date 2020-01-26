e137c6ae8d3a1e919afd54d3e61c0170b1a620a2
chimuramayu <chimuramayu@gmail.com>
2020-01-27 02:08:28 +0900

    [add]投稿機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(id: "DESC")
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to timeline_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -18,5 +27,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:song, :album, :artist, :body, :post_image, :user_id)
   end
 end
