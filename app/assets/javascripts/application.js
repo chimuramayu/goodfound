ac62bc14fcfb38d4699d3b1dc42a985845dacf7c
chimuramayu <chimuramayu@gmail.com>
2020-02-06 21:50:12 +0900

    [add]タイムラインページにもっと見る機能、画像プレビュー機能を追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,27 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+// posts#new 画像を表示
+
+$(function(){
+	$('#file').change(function(e){
+    //ファイルオブジェクトを取得する
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    //画像でない場合は処理終了
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください。");
+      return false;
+    }
+    //アップロードした画像を設定する
+    reader.onload = (function(file){
+      return function(e){
+
+      	console.log(e);
+        $("#img").attr("src", e.target.result);
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
