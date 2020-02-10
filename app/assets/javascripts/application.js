025cb70f726e2f51fbf1e47d9d449fa92f7ca4fd
chimuramayu <chimuramayu@gmail.com>
2020-02-10 16:55:13 +0900

    [fix]ヘッダーを固定、投稿のリンクの範囲を広げる

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,3 +38,9 @@ $(function(){
     reader.readAsDataURL(file);
   });
 });
+
+// ヘッダーの高さ分だけコンテンツを下げる
+$(function() {
+    var height=$("#header").height();
+    $("body").css("margin-top", height);
+});
