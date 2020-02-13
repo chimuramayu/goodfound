4bd6decfafe15fcb7c98fbfa93d79737d1f5d714
chimuramayu <chimuramayu@gmail.com>
2020-02-13 16:01:46 +0900

    [add]スマートフォンサイズのレスポンシブを追加

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '8750606ecbad6df7dd1dbb2e3f7e6b1888fefd9a811c99123a33b3a29ad1e9825c1a7c26c104bfd9f21619a0913b9f583fa2bc33834b40fbd15d81197741196a'
